56de41d5f68e6ddb94e66be3ac97822cc83c0748
mdarshan1810 <mdarshan179@gmail.com>
2026-04-19 20:46:45 +0530

    chore: scaffold package layout and smoke-test unit test wiring

---
+++ app/src/test/kotlin/com/jar/parser/SmokeTest.kt
@@ -0,0 +1,10 @@
+package com.jar.parser
+
+import org.junit.Test
+import org.junit.Assert.assertEquals
+
+class SmokeTest {
+    @Test fun onePlusOneIsTwo() {
+        assertEquals(2, 1 + 1)
+    }
+}
